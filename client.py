71e6df552822b8bb834a0fc2d5732f4cb3f62c81
cw75 <cgwu0530@gmail.com>
2020-05-20 02:54:01 +0000

    server and client impl for nmt

--- client.py
+++ client.py
@@ -43,13 +43,31 @@ if __name__ == '__main__':
 	connect_port = "5556"
 	connect_socket.connect("tcp://%s:%s" % (server_ip, connect_port))
 
-	logging.info('loading images')
-	prefix = 'imagenet'
-	files = os.listdir(prefix)
-	files = [os.path.join(prefix, fname) for fname in files]
-	image_list = []
-	for fname in files:
-		image_list.append(np.array(Image.open(fname).convert('RGB').resize((224, 224))))
+	logging.info('loading sentences')
+
+	french = [
+		'Je m\'appelle Pierre.',
+		'Comment allez-vous aujourd\'hui?',
+		'La nuit est longue et froide, et je veux rentrer chez moi.',
+		'Tu es venue a minuit, mais je me suis déja couché.',
+		'On veut aller dehors mais il faut rester dedans.'
+	]
+
+	german = [
+		'Ich bin in Berliner.',
+		'Die katz ist saß auf dem Stuhl.',
+		'Sie schwimmt im Regen.',
+		'Ich gehe in den Supermarkt, aber mir ist kalt.',
+		'Ich habe nie gedacht, dass du Amerikanerin bist.'
+	]
+
+	english = [
+		'What is the weather like today?',
+		'Why does it rain so much in April?',
+		'I like running but my ankles hurt.',
+		'I should go home to eat dinner before it gets too late.',
+		'I would like to hang out with my friends, but I have to work.'
+	]
 
 	print('listening for trigger')
 	logging.info('listening for trigger')
@@ -72,16 +90,20 @@ if __name__ == '__main__':
 			if request % 10 == 0:
 				print('request number %d' % request)
 				logging.info('request number %d' % request)
+
+			if random.random() < 0.5:
+				other = random.choice(french)
+			else:
+				other = random.choice(german)
+			vals = [other, random.choice(english)]
+
 			start = time.time()
-			img = random.choice(image_list)
-			payload = pa.serialize(['t', img]).to_buffer().to_pybytes()
+			payload = pa.serialize(vals).to_buffer().to_pybytes()
 			connect_socket.send(payload)
 			message = connect_socket.recv()
 			end = time.time()
-			#print(pa.deserialize(message))
+			logging.info('translation is: %s' % pa.deserialize(message))
 			latencies.append((end - start))
-			#print('invocation took %s seconds' % (end - start))
-			#logging.info('invocation took %s seconds' % (end - start))
 
 		if len(latencies) > 200:
 			del latencies[:200]
--- server.py
+++ server.py
@@ -37,29 +37,18 @@ if __name__ == '__main__':
 	logging.info('listening for request')
 
 	while True:
-		payload = socket.recv()
+		inp, english_sentence = pa.deserialize(socket.recv())
 
-		#print('Transform Stage')
-		#logging.info('Transform Stage')
-		#transformed_img = pa.deserialize(invoke('cascade-transform', payload))
-		transformed_img = pa.deserialize(invoke('cascade-transform', payload))
-		#print('Resnet Stage')
-		#logging.info('Resnet Stage')
-		payload = pa.serialize(['r', transformed_img]).to_buffer().to_pybytes()
-		#res_index, res_prob = pa.deserialize(invoke('cascade-resnet', payload))
-		res_index, res_prob = pa.deserialize(invoke('cascade-resnet-gpu', payload))
-		#print('Inception Stage')
-		#logging.info('Inception Stage')
-		payload = pa.serialize(['i', transformed_img, res_prob]).to_buffer().to_pybytes()
-		#ic_index, ic_prob = pa.deserialize(invoke('cascade-inception', payload))
-		ic_index, ic_prob = pa.deserialize(invoke('cascade-inception-gpu', payload))
-		#print('Cascade Stage')
-		#logging.info('Cascade Stage')
-		payload = pa.serialize(['c', res_index, res_prob, ic_index, ic_prob]).to_buffer().to_pybytes()
-		#result = pa.deserialize(invoke('cascade', payload))
-		#socket.send(invoke('cascade-cascade', payload))
-		socket.send(invoke('cascade-cascade', payload))
-		#print(result)
+		payload = pa.serialize(inp).to_buffer().to_pybytes()
 
-	#end = time.time()
-	#print('invocation took %s seconds' % (end - start))
+		logging.info('Classification Stage')
+		language = pa.deserialize(invoke('nmt-c', payload))
+
+		logging.info('Translation Stage')
+		payload = pa.serialize(english_sentence).to_buffer().to_pybytes()
+		if language == 'fr':
+			socket.send(invoke('nmt-f', payload))
+		elif language == 'de':
+			socket.send(invoke('nmt-g', payload))
+		else:
+			logging.info('Unexpected Language Type')
